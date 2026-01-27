7aed3533b76ee3c113ae8ee5eacb2e976c1e4f65
kgyyds <1361906712@qq.com>
2026-01-27 12:50:40 +0800

    测试构建

--- app/src/main/java/com/example/proxypotps/ui/Navigation.kt
+++ app/src/main/java/com/example/proxypotps/ui/Navigation.kt
@@ -4,6 +4,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Storage
+//test
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
